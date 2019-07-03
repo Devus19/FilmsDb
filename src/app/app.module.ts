6304924266a5cff5e1f8a6abd11275ffe922bde1
Devus19 <marekdziwisz69@gmail.com>
2019-07-03 18:59:22 +0200

    add text, button and logo, add responsive styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopMenuComponent } from './top-menu/top-menu.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, TopMenuComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
